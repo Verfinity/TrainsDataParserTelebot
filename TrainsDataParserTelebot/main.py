d4f5595585f00c970f03aa1891d7d3f3a7558677
Verfinity <ilitokarev@gmail.com>
2026-03-22 13:12:00 +0300

    Add bot with minimal logic

--- TrainsDataParserTelebot/main.py
+++ TrainsDataParserTelebot/main.py
@@ -1,16 +1,24 @@
-from exceptions import *
-from train_parser import TrainParser
 import asyncio
+from dotenv import load_dotenv
+from os import getenv
+
+from handlers import router
+
+from aiogram import Bot, Dispatcher
 
 
-async def main():
-    train_parser = TrainParser()
-    try:
-        print(await train_parser.get_train_info('Гомель', 'Минск', '2026-03-29', '749Б'))
-    except IncorrectRequestDataException:
-        print('IncorrectRequestDataException')
-    except IncorrectTrainNumberException:
-        print('IncorrectTrainNumberException')
+load_dotenv()
+TOKEN = getenv('BOT_TOKEN')
+
+
+async def main() -> None:
+    bot = Bot(token=TOKEN)
+
+    dp = Dispatcher()
+    dp.include_router(router)
+
+    await dp.start_polling(bot, skip_updates=True)
+
 
 if __name__ == '__main__':
     asyncio.run(main())
